27e1c59ea03b9afc28189224371e367468c305de
Kyle Payne <knpayne2@illinois.edu>
2015-08-03 15:02:23 -0500

    committed

--- post_samp.R
+++ post_samp.R
@@ -2,7 +2,7 @@
 # This file contains the posterior sampling
 # algorithm and sample size estimator
 # for the bayesian model
-
+source('dph.R')
 
 reject_samp <- function(r, k, m, N, shape1, shape2, C){
   ## rejection sampling 
@@ -21,73 +21,62 @@ reject_samp <- function(r, k, m, N, shape1, shape2, C){
   }
 }
 
-
-
-
-
 post_samp <- function(r, m, N, theta_0, beta, thresh, num_iter){
- 
-  #### -----
-  ## This part is or parallelization of the sampling of the posterior 
-  #create cluster
-  library(parallel)
-  cl <- makeCluster(detectCores()-1)  
-  clusterEvalQ(cl, library(magrittr))
-  #put objects in place that might be needed for the code
-  effect <- thresh - theta_0
-  
+   
+    #### -----
+    ## This part is or parallelization of the sampling of the posterior 
+    #create cluster
+    #library(parallel)
+    #cl <- makeCluster(detectCores()-1, outfile="log4.txt")  
+    #clusterEvalQ(cl, library(magrittr))
+    #put objects in place that might be needed for the code
+    effect <- thresh - theta_0
+    #library(foreach)
+    #library(doParallel)
+    #registerDoParallel(cl)
+    
 
-  
-  
-
-  
- # --- start of algorithm
- 
- # setting the bounds on k
-  if( m - (N-N*theta_0)/k <= 0 ){
-    k_max <- (N-N*theta_0)/m
-  }else{
-    k_max <- (N-N*theta_0)/(m-r)
-  }
- 
- 
- # set shape1 s.t. the prior mode is equal to theta_0
- 
- shape_1 <- (1 + theta_0*beta - 2*theta_0)/(1-theta_0)
- 
- k <- 4:k_max
-  # k <- 1:20
-  
- clusterExport(cl,c("reject_samp", "dph", "r", "m", "N", "theta_0",
-                  "beta", "thresh", "effect", "workerfun"))
- p <- lapply(k, FUN=function(size){
-  rs <- function(size){
-    print("this is the rs function")
-     s <- reject_samp(r,k=size,m,N, 
-                      shape1=2, shape2=beta, C = 0.01)  
-     return(s)
-  }
-   # parSapply performs parallelized sampling od
-   # the posterior distribution specified using
-   # the reject_samp function
-   samps <- parSapply(cl,1:num_iter, rs)
+    
+   # --- start of algorithm
+   
+   # setting the bounds on k
+    if( m - (N-N*theta_0)/k <= 0 ){
+      k_max <- (N-N*theta_0)/m
+    }else{
+      k_max <- (N-N*theta_0)/(m-r) 
+    }
+   
+   
+   # set shape1 s.t. the prior mode is equal to theta_0
+   
+   shape_1 <- (1 + theta_0*beta - 2*theta_0)/(1-theta_0)
+   k <- 2:(k_max - 1)
+   # clusterExport(cl=cl, list("reject_samp", "r", "N", "theta_0", "beta", 
+    #                         "effect", "thresh", "num_iter", "dph", "k"),
+    #             envir=environment())
+   
+    p <- lapply(k, FUN = function(size){
+      # lapply performs parallelized sampling on
+      # the posterior distribution specified using
+      # the reject_samp function
+      samps <- replicate(num_iter, expr=reject_samp(r,k=size,m,N, 
+                           shape1=shape_1, shape2=beta, C = 0.01), simplify="array")
+      rr <- mean(is.na(samps))
+      samps<- samps[-which(is.na(samps))]
+      pow <- mean(samps <= effect) 
+      print(pow)
+      return(pow)
+    })
+   p <- unlist(p)
+   k_maxi <- k[which.max(p)]
+   p_max <- max(p)
+   samps <- reject_samp(r, k_maxi, m, N, shape1=2, shape2=beta, C = 0.01)
+   samps <- samps[-which(is.na(samps))]
    rr <- mean(is.na(samps))
-   samps<- samps[-which(is.na(samps))]
-   print(samps)
-   p <- mean((samps <= effect)) 
-   return(p)
- })
- 
- p <- unlist(p)
- k_maxi <- k[which.max(p)]
- p_max <- max(p)
- samps <- reject_samp(r, k_maxi, m, N, shape1=2, shape2=2, C = 0.01)
- samps <- samps[-which(is.na(samps))]
- rr <- mean(is.na(samps))
- return(list('max seed lot size' = k_maxi, 'power' = p_max, 'samples' = samps,
-              'reject rate' = rr))
-
- stopCluster(cl)
+   return(list('max seed lot size' = k_maxi, 'power' = p_max, 'samples' = samps,
+                'reject rate' = rr))
+  
+   #stopCluster(cl)
 }
 
 ## testing the rejection sampling and sample size estimator
